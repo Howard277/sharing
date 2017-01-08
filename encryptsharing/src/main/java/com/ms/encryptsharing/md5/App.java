c6d615c4d5b6a1b4c91351c53db9153ccdc5ce9b
wuketao277 <454472504@qq.com>
2017-01-08 11:13:14 +0800

    增加md5

---
+++ encryptsharing/src/main/java/com/ms/encryptsharing/md5/App.java
@@ -0,0 +1,35 @@
+package com.ms.encryptsharing.md5;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.Scanner;
+
+/**
+ * MD5
+ * 
+ * @author Administrator
+ *
+ */
+public class App {
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws NoSuchAlgorithmException {
+		while (true) {
+			System.out.println("input:");
+			String str = scanner.nextLine();
+			System.out.println("output:");
+			printMD5(str);
+			System.out.println("---------------------");
+			String next = scanner.nextLine();
+		}
+	}
+
+	private static void printMD5(String txt) throws NoSuchAlgorithmException {
+		MessageDigest md5 = MessageDigest.getInstance("MD5");
+		md5.update(txt.getBytes());
+		byte[] bs = md5.digest();
+		System.out.println(new String(Base64.getEncoder().encode(bs)));
+
+	}
+}
